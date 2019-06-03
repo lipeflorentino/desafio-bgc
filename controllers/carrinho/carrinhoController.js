b3ba5f1e96748f28b7098d2e9d396d6899df120d
lipeflorentino <lipeflorentino2@gmail.com>
2019-06-03 14:36:20 +0000

    atualiza

--- controllers/carrinho/carrinhoController.js
+++ controllers/carrinho/carrinhoController.js
@@ -1,9 +1,16 @@
 console.log('criei o controller de carrinho!');
 
 const Carrinho = require('../../models/carrinho/carrinhoModel.js');
-const User = require('../../models/user/userModel.js');
 
 //metodo do controller para buscar minion por id
-exports.get_user_carrinho_by_id = function (req, res) {
-    
+exports.get_carrinho_by_id = function (req, res) {
+    Carrinho.getCarrinhoById(req, res, function(err, carrinho){
+          if(err){
+            console.log('resultado: ', err);
+            res.send(err);  
+          }else{
+            console.log('resultado: ', carrinho);
+            res.send(carrinho);
+          }
+    });  
 };
